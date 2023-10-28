330d8f4b4f84c742f48d68f619597de541bab6b1
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-10-28 15:44:26 +0530

    start backend logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from watchdog.events import EVENT_TYPE_OPENED
-
+from PIL import Image
+import pytesseract
 app = Flask(__name__)
 
 
@@ -8,6 +9,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route(/upload)
+def upload():
+         if 'image' in request.files:
+            uploaded_image = request.files['image']
+        if uploaded_image.filename != '':
+            image = Image.open(uploaded_image)
+            text = pytesseract.image_to_string(image)
 
 if __name__ == '__main__':
     app.run(debug=True)
