cee920ff5f22d541267629869f199952619b1ec9
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-10-31 01:14:39 +0530

    complete the code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from watchdog.events import EVENT_TYPE_OPENED
 from PIL import Image
 import pytesseract
+from summa import summarizer
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
@@ -25,10 +26,27 @@ def upload():
     return "No image uploaded."
 
 
+@app.route('/generate_summary', methods=['POST'])
+def generate_summary():
+    if 'text' in request.form:
+        input_text = request.form['text']
+        summary = summarizer.summarize(input_text)
+        return redirect(url_for('result', summary=summary))
+
+    return "No text provided."
+
+
+# @app.route('/summary')
+# def summary():
+#     summary = request.args.get('summary')
+#     return render_template('index.html', summary=summary)
+
+
 @app.route('/result')
 def result():
     text = request.args.get('text')
-    return render_template('index.html', extracted_text=text)
+    summary = request.args.get('summary')
+    return render_template('index.html', extracted_text=text, summary=summary)
 
 
 if __name__ == '__main__':
